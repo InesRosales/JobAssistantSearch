a895554952ca55291a060246477f9ac2240af977
Unknown <ines.velez@gmail.com>
2017-06-29 13:41:04 -0500

    Save Job Application Form Button
    Inside the new Job Application Form, a save button was added

--- Android/app/src/main/java/com/example/morningstar/jobsearchassistant/JobApplicationFormActivity.java
+++ Android/app/src/main/java/com/example/morningstar/jobsearchassistant/JobApplicationFormActivity.java
@@ -2,6 +2,10 @@ package com.example.morningstar.jobsearchassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class JobApplicationFormActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class JobApplicationFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_application_form);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_job_application_form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_save:
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
