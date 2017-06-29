bce9ce0fff23c0cc33c9a6918967b08c967769ff
Unknown <ines.velez@gmail.com>
2017-06-28 22:29:22 -0500

    Add Job Button, Job Application Skeleton and Readme
    - An add job application button was added to the job list activity.
    - The skeleton of job application form was created.
    - Readme was updated to contain app description

---
+++ Android/app/src/main/java/com/example/morningstar/jobsearchassistant/JobApplicationFormActivity.java
@@ -0,0 +1,13 @@
+package com.example.morningstar.jobsearchassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JobApplicationFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_application_form);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 JobSearchAssistant
+
+An Android Application that helps the user keep track of job interviews and possible jobs.
